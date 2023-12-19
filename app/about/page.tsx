407bfa36663bf90206c8b6c13fa38b8ddadcec24
NCT <nctrung04102000@gmail.com>
2023-12-19 14:34:06 +0700

    add about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,6 +16,9 @@ export default function About(props: AboutProps) {
     <div>
       About page!
       <button onClick={click}>Back</button>
+      <button onClick={click}>Back</button>
+      <button onClick={click}>Back</button>
+      <button onClick={click}>Back</button>
     </div>
   );
 }
