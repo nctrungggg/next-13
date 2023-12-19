9992e46dc2ddee763fdbd1c00ce6242fc8dee5fb
NCT <nctrung04102000@gmail.com>
2023-12-19 09:22:40 +0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export interface AboutProps {}
+
+export default function About(props: AboutProps) {
+  const router = useRouter();
+
+  const click = () => {
+    console.log("render");
+    router.push("/");
+  };
+
+  return (
+    <div>
+      About page!
+      <button onClick={click}>Back</button>
+    </div>
+  );
+}
